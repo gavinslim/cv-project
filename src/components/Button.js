5c12063be29c2502f02a11056bbe91a52e386ab5
gavin <gavin.lim@alumni.ubc.ca>
2022-04-08 00:04:20 -0700

    Add closeButton function

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,4 @@
 function OpenButton(props) {
-  // const { click } = this.props;
   return (
     <button className='open-btn' onClick={props.click}>
       <i className="fa-solid fa-plus"></i>
@@ -7,7 +6,34 @@ function OpenButton(props) {
   )
 }
 
-module.exports = {
+function CloseButton(props) {
+  return (
+    <button className='close-btn' onClick={props.click}>
+      <i className="fa-solid fa-xmark"></i>
+    </button>
+  )
+}
+
+function SubmitButton(props) {
+  return (
+    <button className='submit-btn' onClick={props.click}>
+      Save
+    </button>
+  )
+}
+
+function EditButton(props) {
+  return (
+    <button className='edit-btn' onClick={props.click}>
+      <i class="fa-solid fa-pencil"></i>
+    </button>
+  )
+}
+
+export {
   OpenButton,
+  CloseButton,
+  SubmitButton,
+  EditButton,
 };
 
